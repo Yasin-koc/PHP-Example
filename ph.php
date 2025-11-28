ed7029f4d1121fb0e962c8bd278996c1b39fd803
YasinKoc <yasin32477@gmail.com>
2025-11-28 18:11:38 +0300

    Add files via upload

---
+++ ph.php
@@ -0,0 +1,17 @@
+<?php
+class ogrenci
+{
+public$adi;
+buclic$bolum;
+public$yas;
+fuction yazdir();
+{ 
+    echo$this->adi ." ".this->bolum ." ".$this->yas;
+     } 
+     } 
+$nesne_ogrenci= new ogrenci;
+$nesne_ogrenci->adi="yasin";
+$nesne_ogrenci"->bolum="bilisim";
+$nesne_ogrenci"->yas=20;
+$nesne_ogrenci"->yazdir();
+?>
